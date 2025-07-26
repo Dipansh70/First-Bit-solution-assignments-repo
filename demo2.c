4317bba4d9f8379f3bd192ea3d4fa173f2f5732d
Dipanshu Sharma <dipanshusharma7015@gmail.com>
2025-07-26 13:33:41 +0530

    Update demo2.c

--- demo2.c
+++ demo2.c
@@ -4,7 +4,7 @@ int main() {
     float radius, area;
     
     printf("Enter radius: ");
-    scanf("%f", &radius);  // Basic input
+    scanf("%f", &radius); 
     
     area = 3.14159 * radius * radius;
     
