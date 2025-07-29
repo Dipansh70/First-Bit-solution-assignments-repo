7b9885b82bc15e2634bba9215ae41d06619ca649
Dipanshu Sharma <dipanshusharma7015@gmail.com>
2025-07-29 23:42:00 +0530

    Question 10. = Answer

---
+++ demo10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+	int a = 56;
+	int b = 76;
+	int c = 89;
+	int d = 76;
+	int e = 149;
+	
+	int res = a+b+c+d+e;
+	
+	int v = res/5;
+	
+	printf("a = %d and v = %d", v, res);
+}
